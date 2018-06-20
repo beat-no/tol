8c144ffe8f74e173c21e86d6b8056908a0a698a2
Øystein S. Haaland <oystein.s.haaland@gmail.com>
2018-06-20 09:08:22 +0200

    Initial commit, add setup.py and the first module: postgres

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tol',
+    version='0.0.1',
+    packages=['tol'],
+    author='Øystein S. Haaland',
+    author_email='oystein@beat.no',
+    description="tøl n1 (norrønt tól 'reiskap') kollektivt: (små)saker",
+    #url='https://gitlab.dev.beat.no/ops/beat-python-utils'
+)
